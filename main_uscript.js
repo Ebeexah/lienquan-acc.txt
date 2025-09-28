81175a6d0a2b536f5575153d09ee99d114509172
Ebeexah <saikomusic09@gmail.com>
2025-09-28 22:35:15 +0700

    Add files via upload

---
+++ main_uscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         zSsaiky Key Auto
+// @namespace    http://tampermonkey.net/
+// @version      0.36.0
+// @description  Ultimate premium
+// @author       Ultimate premium
+// @match        https://muahack.xyz/*
+// @icon         https://github.com/Ebeexah/lienquan-acc.txt/raw/refs/heads/main/IMG_7607.jpeg
+// @require      https://raw.githubusercontent.com/Ebeexah/lienquan-acc.txt/main/key.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
