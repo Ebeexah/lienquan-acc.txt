fbf66df9c2cedbb183abd657b86680e5597cab13
Ebeexah <saikomusic09@gmail.com>
2025-09-28 22:43:52 +0700

    Update main_uscript.js

--- main_uscript.js
+++ main_uscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zSsaiky Key
 // @namespace    http://tampermonkey.net/
-// @version      0.36.0
+// @version      0.36.2
 // @description  Ultimate premium
 // @author       Ultimate premium
 // @match        https://muahack.xyz/*
