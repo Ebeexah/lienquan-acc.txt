df955838a055694670cf9cf4a8454b209831583d
Ebeexah <saikomusic09@gmail.com>
2025-09-30 23:58:24 +0700

    Update main_uscript.js

--- main_uscript.js
+++ main_uscript.js
@@ -5,7 +5,7 @@
 // @description  Ultimate premium
 // @author       Ultimate premium
 // @match        https://muahack.xyz/*
-// @icon         https://github.com/Ebeexah/lienquan-acc.txt/raw/refs/heads/main/IMG_7607.jpeg
+// @icon         https://github.com/Ebeexah/lienquan-acc.txt/raw/refs/heads/main/IMG_7622.jpeg
 // @require      https://github.com/Ebeexah/lienquan-acc.txt/raw/refs/heads/main/key-v4-bypass.js
 // @grant        none
 // @run-at       document-start
